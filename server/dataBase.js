d41882fa744e942df7fc03222eff80163ec5a45e
shane <sdrafahl@iastate.edu>
2017-02-07 02:27:18 -0600

    Added a call to get user data

--- server/dataBase.js
+++ server/dataBase.js
@@ -39,4 +39,23 @@ method.interact = function(json_instructions,callBack){
     }
 }
 
+method.getUser = function(json_user,callBack){
+    var sql = "SELECT * FROM EMPLOYEES WHERE L_NAME = '" + json_user.lname + "' AND F_NAME = '" + json_user.fname + "' AND EMPLOYEE_ID = " + json_user.id ;
+    console.log(sql);
+    this.connection.query(sql, function(err,rows){
+        if(err){
+            throw err;
+        }
+        var data = {
+                last: rows[0].L_NAME,
+                first: rows[0].F_NAME,
+                emp_id: rows[0].EMPLOYEE_ID,
+                perm_string: rows[0].PERM_STRING,
+                status: rows[0].STATUS,
+                pass: rows[0].PASS
+            };
+            return callBack(data);
+    });
+}
+
 module.exports = SQLDataBase;
