e77c413c4f4dbc0bfd7cdf01488edee5e6d079d8
Alech <alech@iastate.edu>
2017-02-22 21:51:22 -0600

    Got server responses working

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/LoginWindow.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/LoginWindow.java
@@ -1,6 +1,4 @@
 package com.example.alec.positive_eating;
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,44 +6,32 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import com.android.volley.Response;
-import com.example.shaneconnect.ShaneConnect;
-
-import org.json.JSONObject;
-
-import static com.example.alec.positive_eating.Singleton_ShaneConnect_Factory.getShaneConnect;
 
 public class LoginWindow extends AppCompatActivity {
-Button LogInButton;
+    Button LogInButton;
     EditText userName;
     EditText password;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_window);
-        setupService();
 
-        LogInButton=(Button)findViewById(R.id.logInButton);
-        password=(EditText)findViewById(R.id.password);
-        userName=(EditText)findViewById(R.id.userName);
-        ShaneConnect username1= getShaneConnect();
-        username1.createAccount("Harrison","Alec","0",0,"abc","8675309",8675309,8675309,new Response.Listener<JSONObject>(){
+        startService(new Intent(this, ShaneConnectService.class));
 
-                @Override
-                public void onResponse(JSONObject response) {
-                    Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
-                }});
+        LogInButton = (Button) findViewById(R.id.logInButton);
+        password = (EditText) findViewById(R.id.password);
+        userName = (EditText) findViewById(R.id.userName);
+
 
         LogInButton.setOnClickListener(
-                new View.OnClickListener()
-                {
-                    public void onClick(View view)
-                    {
+                new View.OnClickListener() {
+                    public void onClick(View view) {
                         Log.v("EditText", userName.getText().toString());
-                        if(userName.getText().toString().equals("alec")){
-                            if(password.getText().toString().equals("abc123")){
+                        if (userName.getText().toString().equals("alec")) {
+                            if (password.getText().toString().equals("abc123")) {
                                 Intent myIntent = new Intent(LoginWindow.this, MainScreen.class); /** Class name here */
                                 LoginWindow.this.startActivity(myIntent);
                             }
@@ -53,7 +39,9 @@ Button LogInButton;
                     }
                 });
     }
-    private void setupService() {
-        startService(new Intent(this, ShaneConnectService.class));
-    }
+
+
+
+
 }
+
--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Schedule.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Schedule.java
@@ -1,13 +1,42 @@
 package com.example.alec.positive_eating;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.android.volley.Response;
+import com.example.shaneconnect.ShaneConnect;
+import org.json.JSONObject;
+import static com.example.alec.positive_eating.Singleton_ShaneConnect_Factory.getShaneConnect;
 
 public class Schedule extends AppCompatActivity {
+   Button Button_a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        Button_a=(Button) findViewById(R.id.Button22);
+
+        Button_a.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        confirmOrder();
+                    }
+
+                });
+
     }
+    private void confirmOrder(){
+        ShaneConnect vista = getShaneConnect();
+        //ShaneConnect vista = new ShaneConnect("http://proj-309-yt-4.cs.iastate.edu:1234", this);
+        vista.addFood("Apples", 10, "Apples", 1, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+        }
 }
