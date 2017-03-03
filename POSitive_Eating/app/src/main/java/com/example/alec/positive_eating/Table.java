8377a8a8628cbcc2d5adaa40ae7df3e3c439654a
Ethan Wieczorek <ethantw@iastate.edu>
2017-03-03 01:41:54 -0600

    added more server connection lines to the table object

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Table.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Table.java
@@ -21,6 +21,7 @@ public class Table {
     private String employeeID;
     private String customerID;
 
+    //Main default table constructor to build a table object
     public void Table(String ID, int xPOS, int yPOS, int Status, String employeeID, String customerID){
         this.ID = ID;
         this.xPos = xPOS;
@@ -28,13 +29,29 @@ public class Table {
         this.Status = Status;
         this.employeeID = employeeID;
         this.customerID = customerID;
+
+        saveTable();
     }
 
+    //This is the default constructor for setting a table when the add table button gets pressed
     public void Table(String ID, int xPOS, int yPOS){
         Table(ID, xPOS, yPOS, 0, null, null);
     }
 
-    public void saveTable(){
+    //This is the default constructor for getting tables from the server if you already have an ID
+    public void Table(String ID){
+        ShaneConnect sc = new ShaneConnect("http://proj-309-yt-4.cs.iastate.edu:", this);
+        sc.getTable(ID, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //call Table(ID, x, y, status, employeeid, customerid) with the retrieved data.
+                TextView tv = (TextView) findViewById(R.id.text_edit);
+                tv.setText(response.toString());
+            }
+        });
+    }
+
+    protected void saveTable(){
         ShaneConnect sc = new ShaneConnect("http://proj-309-yt-4.cs.iastate.edu:", this);
         sc.setTable(ID, xPos, yPos, new Response.Listener<JSONObject>() {
             @Override
@@ -45,26 +62,41 @@ public class Table {
         });
     }
 
+
+    protected String setID(String ID){
+        this.ID = ID;
+        saveTable(); //save the new information to the server
+    }
+
     protected void setX(int xPOS){
         this.xPos = xPOS;
+        saveTable(); //save the new information to the server
     }
 
     protected void setY(int yPOS){
         this.yPos = yPOS;
+        saveTable(); //save the new information to the server
     }
 
     protected void setStatus(int Status){
         this.Status = Status;
+        saveTable(); //save the new information to the server
     }
 
     protected void setEmployeeID(String employeeID){
         this.employeeID = employeeID;
+        saveTable(); //save the new information to the server
     }
 
     protected void setCustomerID(String customerID){
         this.customerID = customerID;
+        saveTable(); //save the new information to the server
     }
 
+
+    protected String getID(){
+        return this.ID;
+    }
 
     protected int getX(){
         return this.xPos;
