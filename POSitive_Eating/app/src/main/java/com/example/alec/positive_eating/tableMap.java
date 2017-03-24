0790046413c823cc528deced8cc58e376207ace6
Ethan Wieczorek <ethantw@iastate.edu>
2017-03-23 20:46:23 -0500

    forgot something in last push

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/tableMap.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/tableMap.java
@@ -37,7 +37,7 @@ public class tableMap extends Activity {
         setContentView(R.layout.activity_table_map);
         mRootLayout = (RelativeLayout) findViewById(R.id.activity_table_map);
 
-        
+
         //TODO get tables add to allthetables
 
 
@@ -63,59 +63,4 @@ public class tableMap extends Activity {
             }
         });
     }
-
-
-    /**
-     * MyTouchListener is the drag and drop listener that allows the user to move a view around another view.
-     * @PARAMS View view: any XML view that isn't the top level.
-     */
-    private final class MyTouchListener implements View.OnTouchListener {
-        public boolean onTouch(View view, MotionEvent event) {
-            final int X = (int) event.getRawX();
-            final int Y = (int) event.getRawY();
-            switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                case MotionEvent.ACTION_DOWN:
-                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                    _xDelta = X - lParams.leftMargin;
-                    _yDelta = Y - lParams.topMargin;
-                    break;
-                case MotionEvent.ACTION_UP:
-                    break;
-                case MotionEvent.ACTION_POINTER_DOWN:
-                    break;
-                case MotionEvent.ACTION_POINTER_UP:
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                    layoutParams.leftMargin = X - _xDelta;
-                    layoutParams.topMargin = Y - _yDelta;
-                    layoutParams.rightMargin = 350;
-                    layoutParams.bottomMargin = 350;
-                    view.setLayoutParams(layoutParams);
-                    System.out.println("X: " + String.valueOf(view.getX()) + ", Y: " + String.valueOf(view.getY())); //debug
-                    break;
-            }
-            mRootLayout.invalidate();
-            return true;
-        }
-    }
-
-//    public void addNewTable(){
-//        ImageView temp = new ImageView(this);
-//        temp.setImageResource(R.drawable.squaretable);
-//        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
-//        temp.setLayoutParams(layoutParams);
-//        temp.setOnTouchListener(new MyTouchListener());
-//        allTables.add(temp);
-//        mRootLayout.addView(temp);
-//    }
-//
-//    public void saveTables(View view){
-//        if(!allTables.isEmpty()) {
-//            Iterator iterator = allTables.iterator();
-//            while (iterator.hasNext()) {
-//                //add to server
-//            }
-//        }
-//    }
 }
