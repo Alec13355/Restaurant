b1bf29d6588fa05323402dd3339346884229eed9
Alech <alech@iastate.edu>
2017-04-14 10:41:21 -0500

    Some progress on Employee order menu

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Employee_Menu.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Employee_Menu.java
@@ -1,300 +1,152 @@
 package com.example.alec.positive_eating;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnChildClickListener;
-import android.widget.ExpandableListView.OnGroupClickListener;
-import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Response;
+
+import org.json.JSONObject;
+
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
-/*
-*@author  http://www.androhub.com/android-expandablelistview/
+import shaneconnect.ShaneConnect;
+
+import static com.example.alec.positive_eating.Singleton_ShaneConnect_Factory.getShaneConnect;
+/**
+ * @author  http://www.androhub.com/android-expandablelistview/
  * Implementing my own data into it.
+ * @author Christian Shinkle
+ * The CustomerOrderMenu class is used to create orders and sumbit them to the server. It includes
+ * a ViewList for a scrollable list of the customer's order, as well as two buttons, one for adding
+ * a new entree to the order and one for confirm the order to be sent to the server.
  */
+public class Employee_Menu extends AppCompatActivity implements View.OnClickListener {
 
+    private static ArrayList<CustomerOrderItem> orderList;
+    private static Integer[] imageId;
 
-
-public class Employee_Menu extends ActionBarActivity {
-    private static ExpandableListView expandableListView;
-    private static ExpandableListAdapter adapter;
-    Button a;
-    int counter, indexholder, groupprev, groupPos, food;
-    String extrastuff;
-    int table;
-    ArrayList<String> thefood = new ArrayList<String>();
-
+    /**
+     * Creates the activity. Sets listeners for buttons and ListViews.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_order_menu);
 
-        a = (Button) findViewById(R.id.Order1);
-        indexholder = 0;
-        counter = 0;
-        extrastuff = "";
-
-        /**
-         * Makes an expandable list view variable sets it to null then calls
-         * setItem() and setListener()
-         */
-
-
-        expandableListView = (ExpandableListView) findViewById(R.id.simple_expandable_listview);
-
-        // Setting group indicator null for custom indicator
-        expandableListView.setGroupIndicator(null);
-
-        setItems();
-        setListener();
-
+        orderList = new ArrayList<CustomerOrderItem>();
+        imageId = new Integer[]{
+                R.drawable.hamburger,
+                R.drawable.fries
+        };
+        Button confirmOrderBut = (Button) findViewById(R.id.confirm_order);
+        Button addItemBut = (Button) findViewById(R.id.add_item);
+        confirmOrderBut.setOnClickListener(this);
+        addItemBut.setOnClickListener(this);
+        ListView list = (ListView) findViewById(R.id.order_list);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                addSide(position);
+            }
+        });
     }
 
-    // Setting headers and childs to expandable listview
-
     /**
-     * Set tiems will make string array lists.
-     * Then it is currently hard coded until we can iterate through the server to set items
-     * and the tables.
-     * The hashMap is also set with each header and children.
+     * Every time activity is resumed, creates new adapter for ListView and updates list of items
+     * ordered.
      */
-    void setItems() {
-
-        // Array list for header
-        ArrayList<String> header = new ArrayList<String>();
-
-        // Array list for child items
-        List<String> child1 = new ArrayList<String>();
-        List<String> child2 = new ArrayList<String>();
-        List<String> child3 = new ArrayList<String>();
-        List<String> child4 = new ArrayList<String>();
-        List<String> child5 = new ArrayList<String>();
-
-        // Hash map for both header and child
-        HashMap<String, List<String>> hashMap = new HashMap<String, List<String>>();
-
-        // Adding headers to list
-        header.add("Table #");
-        header.add("Hamburger");
-        header.add("Chicken Sandwhich");
-        header.add("Fettuccine Alfredo");
-        header.add("Salad");
-
-        for (int i = 1; i < 16; i++) {
-            child1.add("" + i);
+    @Override
+    public void onResume() {
+        super.onResume();
+        String[] tmp = new String[orderList.size()];
+        for(int i =0;i<tmp.length;i++) {
+            tmp[i] = orderList.get(i).toString();
         }
-        // Adding child data
-
-        child2.add("Cheese");
-        child2.add("Tomato");
-        child2.add("Onion");
-        child2.add("Pickles");
-
-
-        // Adding child data
-        child3.add("Cheese");
-        child3.add("Tomato");
-        child3.add("Onion");
-        child3.add("Pickles");
-        // Adding child data
-        child4.add("Extra Cheese");
-        // Adding child data
-        child5.add("Ranch");
-        child5.add("Honey Mustard");
-        child5.add("Cesar");
-        child5.add("French");
-        child5.add("Plain");
-
-        // Adding header and childs to hash map
-        hashMap.put(header.get(0), child1);
-        hashMap.put(header.get(1), child2);
-        hashMap.put(header.get(2), child3);
-        hashMap.put(header.get(3), child4);
-        hashMap.put(header.get(4), child5);
-
-        adapter = new ExpandableListAdapter(Employee_Menu.this, header, hashMap);
-
-        // Setting adpater over expandablelistview
-        expandableListView.setAdapter(adapter);
+        CustomerOrderList adapter = new CustomerOrderList(this, tmp, imageId);
+        ListView orderListView = (ListView) findViewById(R.id.order_list);
+        orderListView.setAdapter(adapter);
     }
-
-    // Setting different listeners to expandablelistview
 
     /**
-     * Sets listeners so when you touch the expandable list parts
+     * Determines which method should be called for each respective button.
+     * @param v
      */
-    void setListener() {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.confirm_order:
+                confirmOrder(); break;
+            case R.id.add_item:
+                addItem(); break;
+            default: break;
+        }
+    }
 
-        // This listener will show toast on group click
-        expandableListView.setOnGroupClickListener(new OnGroupClickListener() {
+    /**
+     * Returns list of orders.
+     * @return
+     */
+    public static ArrayList<CustomerOrderItem> getOrderList() {
+        return orderList;
+    }
 
+    private void addItem() {
+        Intent i = new Intent(this, CustomerEntreeSideList.class);
+        i.putExtra("NEW_ORDER", true);
+        startActivity(i);
+    }
+
+    private void confirmOrder(){
+        ShaneConnect connect = getShaneConnect();
+        ArrayList<String> tmp = new ArrayList<String>();
+        ArrayList<String> options = new ArrayList<String>();
+        String desc = "";
+        for(CustomerOrderItem oi : orderList) {
+            //TODO
+            //desc needs to be some sort of unique identifier
+            //that way, the removeOrder() in SC will work.
+            tmp.add(oi.getEntree());
+            options.add("entree place holder");
+            desc+=oi.toString();
+            desc+='\n';
+            if(oi.getSide()!= null) {
+                tmp.add(oi.getSide());
+                options.add("sides place holder");
+            }
+        }
+        connect.placeOrder(desc, tmp, options, 10, "To Go", new Response.Listener<JSONObject>() {
             @Override
-            public boolean onGroupClick(ExpandableListView listview, View view,
-                                        int group_pos, long id) {
-
-                Toast.makeText(Employee_Menu.this,
-                        "You clicked : " + adapter.getGroup(group_pos),
-                        Toast.LENGTH_SHORT).show();
-                return false;
+            public void onResponse(JSONObject response) {
+                Toast.makeText(getApplicationContext(), "The order was successfully placed!",
+                        Toast.LENGTH_LONG).show();
+                finish();
             }
         });
 
-        // This listener will expand one group at one time
-        // You can remove this listener for expanding all groups
-        expandableListView
-                .setOnGroupExpandListener(new OnGroupExpandListener() {
-
-                    // Default position
-                    int previousGroup = -1;
-
-                    @Override
-                    public void onGroupExpand(int groupPosition) {
-                        if (groupPosition != previousGroup)
-
-                            // Collapse the expanded group
-                            expandableListView.collapseGroup(previousGroup);
-                        previousGroup = groupPosition;
-                    }
-
-                });
-
-        // This listener will show toast on child click
-        /**
-         * This is the toast to show people what they touched.
-         */
-        expandableListView.setOnChildClickListener(new OnChildClickListener() {
-
+        /*
+        ArrayList<String> a = new ArrayList<String>();
+        String b = "mozzarella Sticks";
+        String c = "Mozzarella Sticks";
+        connect.addFood(b, 6, c, 1, a, new Response.Listener<JSONObject>() {
             @Override
-            public boolean onChildClick(ExpandableListView listview, View view,
-                                        int groupPos, int childPos, long id) {
-                Toast.makeText(
-                        Employee_Menu.this,
-                        "You clicked : " + adapter.getChild(groupPos, childPos),
-                        Toast.LENGTH_SHORT).show();
-
-
-                if (groupPos == 0) {
-                    table = Integer.parseInt(adapter.getChild(groupPos, childPos).toString());
-
-                } else if (groupPos == 1) {
-                    if (groupprev != 1) {
-                        extrastuff = "";
-                        fixfood(groupprev, food);
-                        thefood.add(counter, "Hamburger");
-                        counter++;
-                        groupprev = 1;
-
-
-                        food = 0;
-                    }
-                    if (childPos >= 0) {
-                        if (childPos == 0 && food < 26) {
-                            food += childPos + 1;
-                        } else if (childPos == 1 && food < 26) {
-                            food += childPos + 4;
-                        } else if (childPos == 2 && food < 26) {
-                            food += childPos + 5;
-                        } else if (childPos == 3 && food < 26) {
-                            food += childPos + 10;
-                        }
-
-
-                    }
-
-                } else if (groupPos == 2) {
-                    if (groupprev != 2) {
-                        extrastuff = "";
-                        fixfood(groupprev, food);
-                        thefood.add(counter, "Chicken Sandwhich");
-                        counter++;
-                        groupprev = 2;
-                        food = 0;
-                    }
-                    if (childPos >= 0) {
-
-                        if (childPos == 0 && food < 26) {
-                            food += childPos + 1;
-                        } else if (childPos == 1 && food < 26) {
-                            food += childPos + 4;
-                        } else if (childPos == 2 && food < 26) {
-                            food += childPos + 5;
-                        } else if (childPos == 3 && food < 26) {
-                            food += childPos + 10;
-                        }
-
-                    }
-
-                } else if (groupPos == 3) {
-                    if (groupprev != 3) {
-                        extrastuff = "";
-                        fixfood(groupprev, food);
-                        thefood.add(counter, "Fettuccine Alfredo");
-                        counter++;
-                        groupprev = 3;
-
-                        food = 0;
-                    }
-                    if (childPos >= 0) {
-                        food += childPos + 1;
-                    }
-                } else if (groupPos == 4) {
-                    if (groupprev != 4) {
-                        extrastuff = "";
-                        fixfood(groupprev, food);
-                        thefood.add(counter, "Salad");
-                        counter++;
-                        groupprev = 4;
-                        food = 0;
-                    }
-                    if (childPos >= 0) {
-                        if (childPos == 0 && food < 50) {
-                            food += childPos + 1;
-                        } else if (childPos == 1 && food < 50) {
-                            food += childPos + 4;
-                        } else if (childPos == 2 && food < 50) {
-                            food += childPos + 5;
-                        } else if (childPos == 3 && food < 50) {
-                            food += childPos + 10;
-                        } else if (childPos == 4 && food < 50) {
-                            food += childPos + 20;
-                        }
-
-                    }
-                }
-
-
-                return false;
+            public void onResponse(JSONObject response) {
+                Toast.makeText(getApplicationContext(),
+                        response.toString(), Toast.LENGTH_LONG).show();
             }
         });
-        a.setOnClickListener(
-                new View.OnClickListener() {
-                    public void onClick(View view) {
-                        placeorder();
-                    }
-                }
-        );
+        */
     }
 
-    private void placeorder() {
-        //Was used to place orders. Git removed it....
-        //Need to re add shane connect stuff and actually place the order.
-        //TODO
-    }
-
-
-
-    private void fixfood(int groupprev, int food) {
-        //TODO
-        //Used by something git removed it....
-        //Tranverses food and add the stuff to a text string to add it to a sting to be added
-        //To a server
-
+    private void addSide(int position) {
+        Intent i = new Intent(this, CustomerEntreeSideList.class);
+        i.putExtra("ADD_SIDE", position);
+        startActivity(i);
     }
 }
