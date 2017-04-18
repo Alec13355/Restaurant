768fae6b6b733f0c8cee7e516d74818b0b1ef9df
Alech <alech@iastate.edu>
2017-04-18 17:07:38 -0500

    Started fixing schdule

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Employee_Schedule.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Employee_Schedule.java
@@ -3,145 +3,129 @@
  */
 package com.example.alec.positive_eating;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ExpandableListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.android.volley.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import shaneconnect.ShaneConnect;
+
+import static com.example.alec.positive_eating.Singleton_ShaneConnect_Factory.getShaneConnect;
 
 /**
  * This will be the page the cook goes to, to see what needs to be cooked and in the
  * order it was recived.
  */
 public class Employee_Schedule extends AppCompatActivity {
+    private List<String> Employee = new ArrayList<>();
 
-    private static ExpandableListView expandableListView;
-    private static ExpandableListAdapter adapter;
+    int index;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_status);
-        expandableListView = (ExpandableListView) findViewById(R.id.simple_expandable_listview);
+        setContentView(R.layout.activity_schedule);
 
-        // Setting group indicator null for custom indicator
-        expandableListView.setGroupIndicator(null);
+        index = 0;
+        Getsched(index);
+        final ListView listview = (ListView) findViewById(R.id.listview);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
 
-        setItems();
-        setListener();
-
-    }
-
-    // Setting headers and childs to expandable listview
-    void setItems() {
-
-        // Array list for header
-        ArrayList<String> header = new ArrayList<String>();
-
-        // Array list for child items
-        List<String> child1 = new ArrayList<String>();
-        List<String> child2 = new ArrayList<String>();
-        List<String> child3 = new ArrayList<String>();
-        List<String> child4 = new ArrayList<String>();
-        List<String> child5 = new ArrayList<String>();
-
-        // Hash map for both header and child
-        HashMap<String, List<String>> hashMap = new HashMap<String, List<String>>();
-
-        // Adding headers to list
-        header.add("Table #");
-        header.add("Hamburger");
-        header.add("Chicken Sandwhich");
-        header.add("Fettuccine Alfredo");
-        header.add("Salad");
-
-        for(int i =1;i<16;i++) {
-            child1.add(""+i);
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(Employee.get(i));
         }
-        // Adding child data
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
 
-        child2.add("Cheese");
-        child2.add("Tomato");
-        child2.add("Onion");
-        child2.add("Pickles");
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0).setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        //code here
+                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
 
-        // Adding child data
-        child3.add("Cheese");
-        child3.add("Tomato");
-        child3.add("Onion");
-        child3.add("Pickles");
-        // Adding child data
-        child4.add("Extra Cheese");
-        // Adding child data
-        child5.add("Ranch");
-        child5.add("Honey Mustard");
-        child5.add("Cesar");
-        child5.add("French");
-        child5.add("Plain");
-
-        // Adding header and childs to hash map
-        hashMap.put(header.get(0), child1);
-        hashMap.put(header.get(1), child2);
-        hashMap.put(header.get(2), child3);
-        hashMap.put(header.get(3), child4);
-        hashMap.put(header.get(4), child5);
-
-        adapter = new ExpandableListAdapter(Employee_Schedule.this, header, hashMap);
-
-        // Setting adpater over expandablelistview
-        expandableListView.setAdapter(adapter);
+        });
     }
 
-    // Setting different listeners to expandablelistview
-    void setListener() {
+    private class StableArrayAdapter extends ArrayAdapter<String> {
 
-        // This listener will show toast on group click
-        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
 
-            @Override
-            public boolean onGroupClick(ExpandableListView listview, View view,
-                                        int group_pos, long id) {
-
-                Toast.makeText(Employee_Schedule.this,"You clicked : " + adapter.getGroup(group_pos),
-                        Toast.LENGTH_SHORT).show();
-                return false;
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
             }
-        });
+        }
 
-        // This listener will expand one group at one time
-        // You can remove this listener for expanding all groups
-        expandableListView
-                .setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
 
-                    // Default position
-                    int previousGroup = -1;
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
 
-                    @Override
-                    public void onGroupExpand(int groupPosition) {
-                        if (groupPosition != previousGroup)
+    }
 
-                            // Collapse the expanded group
-                            expandableListView.collapseGroup(previousGroup);
-                        previousGroup = groupPosition;
-                    }
 
-                });
 
-        // This listener will show toast on child click
-        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
 
+
+
+    public void Getsched(final int index) {
+        ShaneConnect alpha = getShaneConnect();
+        alpha.getEmployees(index, new Response.Listener<JSONObject>() {
             @Override
-            public boolean onChildClick(ExpandableListView listview, View view,
-                                        int groupPos, int childPos, long id) {
-                Toast.makeText(
-                        Employee_Schedule.this,
-                        "You clicked : " + adapter.getChild(groupPos, childPos),
-                        Toast.LENGTH_SHORT).show();
-                return false;
+            public void onResponse(JSONObject response) {
+                try{
+                    String a = response.getString("name");
+                    String b=  response.getString("address");
+                    Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+
+                    Employee.add(a);
+                    Employee.add(b);
+
+                    Getsched(index+1);
+                } catch (JSONException e) {
+                    return;
+                }
             }
+
         });
     }}
+
