5a047b65f4933848b6ebc2091be1a63c907f3da1
Ethan Wieczorek <ethantw@iastate.edu>
2017-04-19 13:29:11 -0500

    making employee list

---
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/ViewEmployeeList.java
@@ -0,0 +1,13 @@
+package com.example.alec.positive_eating;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewEmployeeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_employee_list);
+    }
+}
--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/employee.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/employee.java
@@ -8,6 +8,10 @@ public class employee {
     String first;
     String last;
     int ID;
+    private String availability;
+    private String phoneNumber;
+    private int rate;
+    private String pass;
 
     employee(String first, String last, int ID){
         this.first = first;
@@ -15,27 +19,89 @@ public class employee {
         this.ID = ID;
     }
 
+    employee(String first, String last, int ID, String availability, String phone, int rate, String pass){
+        this.first = first;
+        this.last = last;
+        this.ID = ID;
+        this.availability = availability;
+        this.phoneNumber = phone;
+        this.rate = rate;
+        this.pass = pass;
+    }
+    /*
+    First Name
+     */
     public void setFirst(String first){
         this.first = first;
-    }
-
-    public void setLast(String last){
-        this.last = last;
-    }
-
-    public void setID(int ID){
-        this.ID = ID;
     }
 
     public String getFirst(){
         return first;
     }
 
+    /*
+    Last Name
+     */
+    public void setLast(String last){
+        this.last = last;
+    }
+
     public String getLast(){
         return last;
     }
 
+    /*
+    Employee ID
+     */
+    public void setID(int ID){
+        this.ID = ID;
+    }
+
     public int getID(){
         return ID;
+    }
+
+    /*
+    Availability
+     */
+    public void setAvailability(String availability){
+        this.availability = availability;
+    }
+
+    public String getAvailability(){
+        return availability;
+    }
+
+    /*
+    Phone Number
+     */
+    public void setPhone(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String  getPhone(){
+        return phoneNumber;
+    }
+
+    /*
+    Pay Rate
+     */
+    public void setRate(int rate){
+        this.rate = rate;
+    }
+
+    public int getRate(){
+        return rate;
+    }
+
+    /*
+    Password
+     */
+    public void setPassword(String pass){
+        this.pass = pass;
+    }
+
+    public String getPassword(){
+        return pass;
     }
 }
