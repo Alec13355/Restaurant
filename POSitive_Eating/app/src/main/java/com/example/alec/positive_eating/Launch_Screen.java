97493e3a64b95e89c49441f3fa256d98805541f7
Ethan Wieczorek <ethantw@iastate.edu>
2017-04-20 18:46:15 -0500

    someone deleted my back button functionality

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Launch_Screen.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Launch_Screen.java
@@ -1,11 +1,13 @@
 package com.example.alec.positive_eating;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.app.AlertDialog;
 
 import com.example.alec.positive_eating.CustomerLogin.*;
 
