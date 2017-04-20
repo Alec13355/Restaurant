eb1d79fbb7e96fb32c1a1a6812620cb0facf4668
Ethan Wieczorek <ethantw@iastate.edu>
2017-04-20 03:12:31 -0500

    fixed launch screen

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Launch_Screen.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Launch_Screen.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.alec.positive_eating.CustomerLogin.CustomerLoginActivity;
+
 public class Launch_Screen extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
