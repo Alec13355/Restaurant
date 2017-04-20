16b3400139fcd6ff9f8761f867590ea3792f9f53
shane <sdrafahl@iastate.edu>
2017-04-20 11:48:48 -0500

    fixed login screen for customer

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Launch_Screen.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Launch_Screen.java
@@ -1,13 +1,12 @@
 package com.example.alec.positive_eating;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.alec.positive_eating.CustomerLogin.CustomerLoginActivity;
 
 public class Launch_Screen extends AppCompatActivity implements View.OnClickListener {
     @Override
