3a91620a089dc08bd79870a2a3b14b46de8abdfe
Christian <cshinkle@iastate.edu>
2017-04-23 16:53:18 -0500

    Added functionality to Employee_Menu

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/CustomerOrderMenu.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/CustomerOrderMenu.java
@@ -56,18 +56,6 @@ public class CustomerOrderMenu extends AppCompatActivity implements View.OnClick
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (requestCode == 1) {
-            if(resultCode == Activity.RESULT_OK){
-                String result=data.getStringExtra("result");
-            }
-            if (resultCode == Activity.RESULT_CANCELED) {
-                //Write your code if there's no result
-            }
-        }
-    }
     /**
      * Every time activity is resumed, creates new adapter for ListView and updates list of items
      * ordered.
