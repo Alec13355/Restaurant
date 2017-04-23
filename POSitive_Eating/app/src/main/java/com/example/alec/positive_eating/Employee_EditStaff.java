99bcc25eb7dbb3cc13f1646861e5e15e25eebd21
Alech <alech@iastate.edu>
2017-04-22 21:31:04 -0500

    Fixed Schedule so there are no more random crashes.

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Employee_EditStaff.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Employee_EditStaff.java
@@ -1,55 +1,181 @@
-/**
- * @author Alec
- */
 package com.example.alec.positive_eating;
 
-import android.content.Intent;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ExpandableListView;
-import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.alec.positive_eating.payrole.AssistantDirector;
-import com.example.alec.positive_eating.payrole.ConcreteListViewBuilderUsers;
+import com.android.volley.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import shaneconnect.ShaneConnect;
+
+import static android.view.Gravity.CENTER;
 import static com.example.alec.positive_eating.Singleton_ShaneConnect_Factory.getShaneConnect;
 
-/**
- * This class will be used to manage and get current workers.
- */
-public class Employee_EditStaff extends ActionBarActivity {
-    private static ExpandableListView expandableListView;
-    private static ExpandableListAdapter adapter;
-    private Button add;
+public class Employee_EditStaff extends AppCompatActivity {
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listDataChild;
+    private ArrayList<JSONObject> orders;
+    private ShaneConnect ModelM;
+    private int recursiveInc;
+    private int bufferOrderToDelete;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_staff);
-        LinearLayout list1 = (LinearLayout) findViewById(R.id.listOStuff);
-        ConcreteListViewBuilderUsers builder  = new ConcreteListViewBuilderUsers(list1,this);
-        final AssistantDirector dir = new AssistantDirector(getShaneConnect(),builder);
-        dir.directDisplay();
+        setContentView(R.layout.activity_cook_order_list);
+        context = getApplicationContext();
+        bufferOrderToDelete = -1;
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        prepareListData();
+    }
 
-        Button add = (Button) findViewById(R.id.add_employee);
-        //Initilizes the buttons.
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+        getAllOrders();
+    }
 
-        add.setOnClickListener(
-                new View.OnClickListener() {
-                    public void onClick(View view) {
-                        Intent myIntent = new Intent(Employee_EditStaff.this, Employee_add_employee.class); /** Class name here */
-                        Employee_EditStaff.this.startActivity(myIntent);
+    private void setup() {
+        ExpandableListView expListView = (ExpandableListView) findViewById(R.id.lvExp);
+        CookListAdapter listAdapter = new CookListAdapter(context, listDataHeader, listDataChild);
+        expListView.setAdapter(listAdapter);
+
+        TextView text = new TextView(this);
+        text.setText(String.valueOf("Are you sure this order is complete?"));
+        text.setGravity(CENTER);
+
+        final AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setTitle("Finished With Order?");
+        builder.setView(text);
+
+        builder.setPositiveButton("Yes (Delete order)", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                beginRemoveOrder();
+            }
+        });
+        builder.setNegativeButton("No (Go back)", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView listview, View view, int groupPos,
+                                        int childPos, long id) {
+                bufferOrderToDelete = groupPos;
+                builder.show();
+                return false;
+            }
+        });
+    }
+
+    private void beginRemoveOrder() {
+        String descriptions = null;
+        try {
+            descriptions = orders.get(bufferOrderToDelete).getString("desc");
+        } catch (Exception e){
+            Toast.makeText(context, "A very weird error occurred in beginRemoveOrder...",
+                    Toast.LENGTH_LONG).show();
+        }
+        if(bufferOrderToDelete<0 || descriptions==null)return;
+        ModelM.removeOrder(descriptions, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // TODO: 4/20/2017
+            }
+        });
+    }
+
+    private void processOrders(final int i) {
+        try {
+            String compString = orders.get(i).getString("componentString");
+            final String orderNum = "Order #" + orders.get(i).getInt("order_id");
+            ModelM.getFoodByID(compString, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        ArrayList<String> orderInfo = parseResponse(response);
+                        listDataHeader.add(orderNum);
+                        listDataChild.put(orderNum, orderInfo);
+                        if (i == orders.size() - 1) {
+                            setup();
+                        } else {
+                            processOrders(i + 1);
+                        }
+                    } catch (Exception e) {
+                        Toast.makeText(context, "An error occurred in getFoodByID" +
+                                        "Please press the back button and try again.",
+                                Toast.LENGTH_LONG).show();
                     }
                 }
-                /*Displays who is owed money */
+            });
+        } catch (Exception e) {
+            Toast.makeText(context, "An error occurred in getOrders." +
+                            "Please press the back button and try again.",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
 
+    private void getAllOrders() {
+        ModelM = getShaneConnect();
+        recursiveInc = 0;
+        orders = new ArrayList<JSONObject>();
+        ModelM.getOrders(recursiveInc, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse (JSONObject response) {
+                if (response.has("none")) {
+                    processOrders(0);
+                } else {
+                    orders.add(response);
+                    ModelM.getOrders(++recursiveInc, this);
+                }
+            }
+        });
+    }
 
-
-
-        );
+    private ArrayList<String> parseResponse(JSONObject res) throws JSONException {
+        ArrayList<String> order = new ArrayList<String>();
+        for(int i =0;res.has("NAME"+i);i++) {
+            try {
+                String name = "";
+                name+=res.getString("DESCR" + i)+"\n";
+                name+="Quantity:" + res.getInt("QUANTITY" + i)+"\n";
+                name+="Price:$" + res.getInt("PRICE" + i)+"\n";
+                if(res.has("OPTIONS"+i)) {
+                    JSONObject jsonOptions = res.getJSONObject("OPTIONS" + i);
+                    name += "Options:";
+                    int j;
+                    for (j = 0; jsonOptions.has("OPTION" + j); j++) {
+                        name += "\n\t"+ jsonOptions.getString("OPTION" + j);
+                    }
+                    if(j==0)
+                        name+="\n (None)";
+                }
+                //TODO
+                order.add(name);
+            } catch (Exception e) {
+                throw e;
+            }
+        }
+        return order;
     }
 }
-
--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Employee_Schedule.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/Employee_Schedule.java
@@ -5,12 +5,7 @@ package com.example.alec.positive_eating;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnChildClickListener;
-import android.widget.ExpandableListView.OnGroupClickListener;
-import android.widget.ExpandableListView.OnGroupExpandListener;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -47,7 +42,7 @@ public class Employee_Schedule extends AppCompatActivity {
 
 
         setItems();
-        setListener();
+
 
     }
 
@@ -75,10 +70,10 @@ public class Employee_Schedule extends AppCompatActivity {
 
 
         // Adding header and childs to hash map
-        hashMap.put(header.get(0), child1);
-        hashMap.put(header.get(1), child2);
-        hashMap.put(header.get(2), child3);
-        hashMap.put(header.get(3), child4);
+        for(int i =0; i<header.size();i++){
+            hashMap.put(header.get(i),child1);
+        }
+
 
         adapter = new ExpandableListAdapter(Employee_Schedule.this, header, hashMap);
 
@@ -87,53 +82,6 @@ public class Employee_Schedule extends AppCompatActivity {
     }
 
     // Setting different listeners to expandablelistview
-    void setListener() {
 
-        // This listener will show toast on group click
-        expandableListView.setOnGroupClickListener(new OnGroupClickListener() {
 
-            @Override
-            public boolean onGroupClick(ExpandableListView listview, View view,
-                                        int group_pos, long id) {
-
-                Toast.makeText(Employee_Schedule.this,
-                        "You clicked : " + adapter.getGroup(group_pos),
-                        Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-
-        // This listener will expand one group at one time
-        // You can remove this listener for expanding all groups
-        expandableListView
-                .setOnGroupExpandListener(new OnGroupExpandListener() {
-
-                    // Default position
-                    int previousGroup = -1;
-
-                    @Override
-                    public void onGroupExpand(int groupPosition) {
-                        if (groupPosition != previousGroup)
-
-                            // Collapse the expanded group
-                            expandableListView.collapseGroup(previousGroup);
-                        previousGroup = groupPosition;
-                    }
-
-                });
-
-        // This listener will show toast on child click
-        expandableListView.setOnChildClickListener(new OnChildClickListener() {
-
-            @Override
-            public boolean onChildClick(ExpandableListView listview, View view,
-                                        int groupPos, int childPos, long id) {
-                Toast.makeText(
-                        Employee_Schedule.this,
-                        "You clicked : " + adapter.getChild(groupPos, childPos),
-                        Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-    }
 }
