aa7dc90470b22472db94f526436e608f2559e51f
Ethan Wieczorek <ethantw@iastate.edu>
2017-04-23 17:07:50 -0500

    employee can't delete self

--- POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/employee.java
+++ POSitive_Eating/app/src/main/java/com/example/alec/positive_eating/employee.java
@@ -252,7 +252,7 @@ public class employee {
         spinnerList.add("Schedule");
         spinnerList.add("Phone Number");
         spinnerList.add("Password");
-        if(getEInstance().getEmployee().getID() == ID) {
+        if(getEInstance().getEmployee().getID() != ID) {
             spinnerList.add("DELETE EMPLOYEE");
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, spinnerList);
@@ -469,7 +469,8 @@ public class employee {
             }
         });
 
-
+        tempDetails.setText("DELETED");
+        passwordView.setText("DELETED");
     }
 
     //TODO save employee updates to database
